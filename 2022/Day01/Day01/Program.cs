fdeda33826e9ce56593fbd72a9839efaad35965e
Racoonen <40060204+Racoonen@users.noreply.github.com>
2022-12-05 20:05:21 +0100

    Day 01

---
+++ 2022/Day01/Day01/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    static int currentMax;
+    static int currentSecondMax;
+    static int currentThirdMax;
+
+    private static void Main(string[] args)
+    {
+        var current = 0;
+        foreach (var line in File.ReadLines("../../../Input.txt"))
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                SetCurrentMax(current);
+                current = 0;
+                continue;
+            }
+            current += int.Parse(line);
+        }
+
+        Console.WriteLine(currentMax);
+        Console.WriteLine(currentSecondMax);
+        Console.WriteLine(currentThirdMax);
+        Console.WriteLine(currentMax + currentSecondMax + currentThirdMax);
+    }
+
+    private static void SetCurrentMax(int value)
+    {
+        if(value > currentMax)
+        {
+            currentThirdMax = currentSecondMax;
+            currentSecondMax = currentMax;
+            currentMax = value;
+        }
+        else if (value < currentMax && value > currentSecondMax)
+        {
+            currentThirdMax = currentSecondMax;
+            currentSecondMax = value;
+        }
+        else if(value < currentSecondMax && value > currentThirdMax)
+        {
+            currentThirdMax = value;
+        }
+    }
+}
