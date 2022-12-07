649cfcbe96357e3cef2255dc114ee266f7b2d279
Racoonen <40060204+Racoonen@users.noreply.github.com>
2022-12-07 18:13:30 +0100

    Day 3+4

---
+++ 2022/Day03/Day03/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+internal class Program
+{
+
+    private static int TwoElfGroup = 0;
+    private static int ThreeElfGroup = 0;
+
+    private static void Main(string[] args)
+    {
+        foreach(var line in File.ReadAllLines("../../../Input.txt"))
+        {
+            DoTwoElfGroup(line);
+            DoThreeGroup(line);
+        }
+
+        Console.WriteLine(TwoElfGroup);
+        Console.WriteLine(ThreeElfGroup);
+    }
+
+    private static int threeElfGroupCounter = 0;
+    private static string firstLine;
+    private static string secondLine;
+
+    private static void DoThreeGroup(string line)
+    {
+        switch(threeElfGroupCounter % 3)
+        {
+            case 0: firstLine = line; break;   
+            case 1: secondLine = line;break;
+            case 2: ThreeElfGroup += GetCommonItem(firstLine, secondLine, line); break;
+        }
+        threeElfGroupCounter++;
+    }
+
+    private static void DoTwoElfGroup(string line)
+    {
+        var lines = line.Chunk(line.Length / 2);
+        var first = new string(lines.ElementAt(0));
+        var second = new string(lines.ElementAt(1));
+
+        TwoElfGroup += GetCommonItem(first, second);
+    }
+
+    private static int GetCommonItem(params string[] items)
+    {
+        var result = items[0];
+        foreach(var line in items.Skip(1))
+        {
+            result = new string(result.Intersect(line).ToArray());
+        }
+        return GetValue(result[0]);
+    }
+
+    private static int GetValue(char item) =>
+        item switch
+        {
+            >= 'a' and <= 'z' => item - 96,
+            >= 'A' and <= 'Z' => item - 38,
+            _ => throw new Exception(),
+        };
+}
---
+++ 2022/Day04/Day04/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static int sequenceInCounter;
+    private static int overlappingCounter; 
+    private static void Main(string[] args)
+    {
+        foreach(var line in File.ReadAllLines("../../../Input.txt"))
+        {
+            var comparts = line.Split(',');
+            var first = Parse(comparts[0]);
+            var second = Parse(comparts[1]);
+            if (SequenceIn(first, second))
+            {
+                sequenceInCounter++;
+                overlappingCounter++;
+            }
+            else if (Overlapping(first, second))
+                overlappingCounter++;
+        }
+        Console.WriteLine(sequenceInCounter);
+        Console.WriteLine(overlappingCounter);
+    }
+
+    private static bool Overlapping((int start, int last) first, (int start, int last) second)
+        => (first.start >= second.start && first.start <= second.last) || 
+        (second.start >= first.start && second.start <= first.last);
+
+    private static bool SequenceIn((int start, int last) first, (int start, int last) second)
+        => (first.start >= second.start && first.last <= second.last) ||
+        (second.start >= first.start && second.last <= first.last);
+
+    private static (int start, int last) Parse(string input)
+    {
+        var minus = input.IndexOf('-');
+        var first = int.Parse(input.Substring(0, minus));
+        var second = int.Parse(input.Substring(minus + 1));
+        return (first,second);
+    }
+}
