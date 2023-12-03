04bea3118d19420aff0b1d13c50e701452fe6df9
Racoonen <40060204+Racoonen@users.noreply.github.com>
2023-12-03 16:10:18 +0100

    Day03
    Part 2 doenst work. Dont know why.

---
+++ 2023/AoC23/Day03/Parser.cs
@@ -0,0 +1,9 @@
+﻿namespace Day03
+{
+    internal class Parser
+    {
+        public Matrix Parse(string[] lines) => new Matrix(lines.Select(ParseLine).ToArray());
+
+        private Entry[] ParseLine(string line) => line.Select(e => new Entry(e)).ToArray();
+    }
+}
---
+++ 2023/AoC23/Day03/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day03;
+
+var parser = new Parser();
+var matrix = parser.Parse(File.ReadAllLines("./Input.txt"));
+
+Console.WriteLine("First Part.");
+Console.WriteLine($"Sum of valid Parts is {matrix.CalcSumOfValidEntries()}");
+
+
+Console.WriteLine("Second Part.");
+Console.WriteLine($"Sum of Gear Ratio is {matrix.CalcGearRatio()}");
